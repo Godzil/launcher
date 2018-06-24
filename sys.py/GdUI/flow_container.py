b0e66ace54e01c3b13077c9455b91c091811efac
Godzil <godzil@godzil.net>
2018-06-24 22:39:22 +0100

    Add top_margin to flow_container

--- sys.py/GdUI/flow_container.py
+++ sys.py/GdUI/flow_container.py
@@ -6,19 +6,20 @@ from .container import Container
 
 
 class FlowContainer(Container):
-    def __init__(self, x, y, width=10, height=10, bg_color=None, margin=2):
+    def __init__(self, x, y, width=10, height=10, bg_color=None, margin=2, top_margin=2):
         super().__init__(x=x, y=y, width=width, height=height, bg_color=bg_color)
 
         self._LeftChilds = []
         self._RightChilds = []
         self._Margin = margin
+        self._TopMargin = top_margin
 
     def __set_child_pos(self):
         # Left childs
         x_pos = 0
         for c in self._LeftChilds:
             x_pos += self._Margin
-            c.set_position(x_pos, 1)
+            c.set_position(x_pos, self._TopMargin)
             x_pos += c.get_rect().width
 
         # Right childs
@@ -26,10 +27,13 @@ class FlowContainer(Container):
         for c in reversed(self._RightChilds):
             x_pos -= c.get_rect().width
             x_pos -= self._Margin
-            c.set_position(x_pos, 1)
+            c.set_position(x_pos, self._TopMargin)
 
     def set_margin(self, margin):
         self._Margin = margin
+
+    def set_top_margin(self, margin):
+        self._TopMargin = margin
 
     def add_left_child(self, child: Widget):
         self._LeftChilds.append(child)
@@ -38,6 +42,12 @@ class FlowContainer(Container):
     def add_right_child(self, child: Widget):
         self._RightChilds.append(child)
         child.set_parent(self)
+
+    def clear_left_childs(self):
+        self._LeftChilds.clear()
+
+    def clear_right_childs(self):
+        self._RightChilds.clear()
 
     def Draw(self):
         self.__set_child_pos()
